4ca2be0d8a612441f19c37c5bc5302713f415f8c
Elis Hirwing <elis@hirwing.se>
2023-10-06 21:12:46 +0200

    feat(flake): Add the initial flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "jsonresume-nix";
+
+  inputs = {
+    flake-utils.url = "flake-utils";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    {
+      # Flake outputs
+    }
+    // flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # Specify formatter package for "nix fmt ." and "nix fmt . -- --check"
+      formatter = pkgs.alejandra;
+
+      # Set up nix develop shell environment
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.resumed
+          pkgs.nodejs
+        ];
+      };
+    });
+}
